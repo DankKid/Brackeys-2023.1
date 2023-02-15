32f330c1c391b8a80b382a947e87c18d8c0fd500
DankKid <jmanholt929@gmail.com>
2023-02-15 18:12:57 -0500

    Audio Works

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -7,16 +7,58 @@ public class MusicController : MonoBehaviour
 
     [SerializeField] List<AudioClip> audios;
     [SerializeField] AudioSource source;
-    List<AudioClip> currentAudios;
+    List<AudioClip> currentAudios = new List<AudioClip>();
+    int index;
     private void Start()
     {
-        currentAudios = audios;
-
-        for(int i =0; i < currentAudios.Count; i++)
+        for(int i = 0; i < audios.Count - 1; i++)
         {
-            Debug.Log(currentAudios[i].name);
+            currentAudios.Add(audios[i]);
         }
+        
+        index = randomInt();
+        source.clip = currentAudios[index];
+        source.Play();
+
+        
     }
 
+    private void Update()
+    {
+        if (!source.isPlaying)
+        {
+            Debug.Log("Stopped!");
+            NextSong();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            NextSong();
+    }
+
+    private int randomInt()
+    {
+        int value = Random.Range(0, currentAudios.Count - 1);
+        return value;
+    }
+
+
+    void NextSong()
+    {
+        Debug.Log("Next");
+
+
+
+
+        currentAudios.RemoveAt(index);
+        if (currentAudios.Count == 0)
+        {
+            for (int i = 0; i < audios.Count - 1; i++)
+            {
+                currentAudios.Add(audios[i]);
+            }
+        }
+        index = randomInt();
+        source.clip = currentAudios[index];
+        source.Play();
+    }
 
 }
