fb8cc32b3c31855ae5f7405deb647ec81727fec6
DankKid <jmanholt929@gmail.com>
2023-02-16 23:02:31 -0500

    Update PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,12 +7,18 @@ using UnityEngine.UI;
 public class PlayerManager : MonoBehaviour
 {
     int p1Money, p1Wave, p2Money, p2Wave, p3Money, p3Wave;
-    public int currentPhase;
+    public int currentPhase, currentDollar;
 
     [SerializeField] TMP_Text currentMoney, finalScore, bestScoreText;
     [SerializeField] Image currentCurrency;
     [SerializeField] List<Sprite> currencies = new List<Sprite>();
-    void NextPhase()
+    [SerializeField] Light globalLight;
+    [SerializeField] GameObject gameOverScreen;
+
+    Color zombieLight = new Color(0,202,255,255);
+    Color machineLight = new Color(255,89,0,255);
+    Color defaultLight = new Color(255, 255, 255, 255);
+    public void NextPhase()
     {
         if(currentPhase < 3)
         {
@@ -22,14 +28,41 @@ public class PlayerManager : MonoBehaviour
         }
         else
         {
+            p3Money = currentDollar;
+            currentDollar = 0;
             gameOver();
         }
+
+
+        if(currentPhase == 2)
+        {
+            globalLight.color = zombieLight;
+            p1Money = currentDollar;
+            currentDollar = 0;
+
+        }else if(currentPhase == 3)
+        {
+            globalLight.color = machineLight;
+            p2Money = currentDollar;
+            currentDollar = 0;
+
+        }
+
+
     }
 
     void gameOver()
     {
-
+        calculateScore();
+        gameOverScreen.SetActive(true);
     }
+
+    public void getMoney(int value)
+    {
+        currentDollar += value;
+    }
+
+
 
     void calculateScore()
     {
