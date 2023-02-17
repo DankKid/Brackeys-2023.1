8ca79b1635a120c341e5b35960c5c0f33ab476d9
Connor <61102529+Metater@users.noreply.github.com>
2023-02-17 09:25:19 -0500

    Zambo can move

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
+    [SerializeField] private float lurchConstant;
+    [SerializeField] private float walkSpeed;
+
     // lurch forward, move forward in bursts
 
     // damage indicator could be slowly fading placeable to black based off of percent health
@@ -25,5 +28,12 @@ public class Attacker : MonoBehaviour
         grid = FindObjectOfType<PlaceableGrid>();
 
         SetY(1);
+    }
+
+    private void Update()
+    {
+        Vector3 position = transform.position;
+        position.x -= Time.deltaTime * (walkSpeed + Mathf.Pow(transform.position.x % 1f, 1f / lurchConstant));
+        transform.position = position;
     }
 }
