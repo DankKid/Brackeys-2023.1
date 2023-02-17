e80492513584974f5b25c58d4989ec821d8dceba
Connor <61102529+Metater@users.noreply.github.com>
2023-02-16 22:56:00 -0500

    The generator generates now

--- Assets/Scripts/Placeables/GeneratorPlaceable.cs
+++ Assets/Scripts/Placeables/GeneratorPlaceable.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class GeneratorPlaceable : Placeable
 {
+    [SerializeField] private float generationCooldown;
+    [SerializeField] private Transform currencySpawnTransform;
+    [SerializeField] private Transform currenciesTransform;
+    [SerializeField] private Currency currencyPrefab;
+
     [SerializeField] private Vector2 wiggleRange;
     [SerializeField] private float wiggleFrequency;
 
+    private float timeUntilGeneration;
+
+    protected override void ProtectedAwake()
+    {
+        timeUntilGeneration = generationCooldown;
+    }
+
     protected override void PlacedUpdate()
     {
+        timeUntilGeneration -= Time.deltaTime;
+
+        // Wiggle
         float zRotation = Mathf.LerpAngle(wiggleRange.x, wiggleRange.y, (Mathf.Sin(TimeSincePlacement * wiggleFrequency * Mathf.PI * 2f) + 1f) / 2f);
         spriteRenderer.transform.localEulerAngles = new Vector3(0, 0, zRotation);
+
+        // Generation
+        if (timeUntilGeneration <= 0)
+        {
+            timeUntilGeneration += generationCooldown;
+            Instantiate(currencyPrefab, currencySpawnTransform.position, Quaternion.identity, currenciesTransform);
+        }
     }
 }
