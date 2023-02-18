18381a54575486adc60bae276ba925bb35c13f35
Connor <61102529+Metater@users.noreply.github.com>
2023-02-17 22:33:24 -0500

    Attacker work

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [SerializeField] private float lurchConstant;
-    [SerializeField] private float walkSpeed;
-
-    // lurch forward, move forward in bursts
-
-    // damage indicator could be slowly fading placeable to black based off of percent health
+    [SerializeField] private float walkConstant;
+    [SerializeField] private float speed;
+    [SerializeField] private float movementCooldown;
 
     private PlaceableGrid grid;
+    private float timeUntilMovement = 0;
+
+    // https://www.desmos.com/calculator/ixjzuo7i4g
 
     // 1-5
-    public void SetY(int y)
+    public void SetPosition(float x, int y)
     {
         float rowY = grid.GetCellCenter(new Vector2Int(0, y)).y;
 
         Vector2 position = transform.position;
+        position.x = x;
         position.y = rowY;
         transform.position = position;
     }
@@ -27,13 +29,29 @@ public class Attacker : MonoBehaviour
     {
         grid = FindObjectOfType<PlaceableGrid>();
 
-        SetY(1);
+        SetPosition(10f, 1);
     }
 
     private void Update()
     {
-        Vector3 position = transform.position;
-        position.x -= Time.deltaTime * (walkSpeed + Mathf.Pow(transform.position.x % 1f, 1f / lurchConstant));
-        transform.position = position;
+        if (timeUntilMovement <= 0)
+        {
+            Vector3 position = transform.position;
+            position.x -= Time.deltaTime * speed * (walkConstant + Mathf.Pow((transform.position.x + 1024f) % 1f, 1f / lurchConstant));
+            transform.position = position;
+        }
+
+        timeUntilMovement -= Time.deltaTime;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Placeable placeable = collision.gameObject.GetComponentInParent<Placeable>();
+        if (placeable == null)
+        {
+            return;
+        }
+
+        timeUntilMovement = movementCooldown;
     }
 }
